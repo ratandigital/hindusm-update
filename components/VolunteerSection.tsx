6d3b443e7032bef006c386ea8dcd2a3e60f86025
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2025-03-09 13:21:34 +0600

    Update VolunteerSection.tsx

--- components/VolunteerSection.tsx
+++ components/VolunteerSection.tsx
@@ -1,36 +1,31 @@
 "use client";
 
 import Slider from "react-slick";
-import React from 'react'
-import VolunteerSlider from "./VolunteerSlider";
+import React from "react";
 import VolunterrSocialTegger from "./VolunterrSocialTegger";
 import dynamic from "next/dynamic";
 
-// Dynamically import VolunteerSlider with SSR disabled
-const VolunteerSlider = dynamic(() => import("./VolunteerSlider"), { ssr: false });
+// Dynamically import the VolunteerSlider with SSR disabled
+const DynamicVolunteerSlider = dynamic(() => import("./VolunteerSlider"), { ssr: false });
 
 export default function VolunteerSection() {
-   
   return (
     <section className="section section-padding light-bg">
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-lg-6 mb-lg-30">
-          <div className="section-title mb-0 text-start">
-            <p className="subtitle">volunteers</p>
-            <h4 className="title">Our Volunteers Achievments</h4>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6 mb-lg-30">
+            <div className="section-title mb-0 text-start">
+              <p className="subtitle">volunteers</p>
+              <h4 className="title">Our Volunteers Achievements</h4>
+            </div>
+            <p className="blockquote bg-transparent">
+              We are a Hindu that believes in Lord Rama and Vishnu Deva, and we are a Hindu that believes in Lord Rama and Vishnu Deva.
+            </p>
+            <DynamicVolunteerSlider />
           </div>
-          <p className="blockquote bg-transparent">
-            {" "}
-            We are a Hindu that belives in Lord Rama and Vishnu Deva the
-            followers and We are a Hindu that belives in Lord Rama and Vishnu
-            Deva.{" "}
-          </p>
-          <VolunteerSlider/>
+          <VolunterrSocialTegger />
         </div>
-     <VolunterrSocialTegger/>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
